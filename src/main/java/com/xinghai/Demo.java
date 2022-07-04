5a2bfbf7efce9afc70fd598b0521b6b7902db4e6
nydd <2104774001@qq.com>
2022-07-04 20:19:52 +0800

    第一次提交

---
+++ src/main/java/com/xinghai/Demo.java
@@ -0,0 +1,11 @@
+package com.xinghai;
+
+/**
+ * @author zg
+ * @version 1.0
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("世界,你好!!!");
+    }
+}
